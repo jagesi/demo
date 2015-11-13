368f82c6b0002d44bac37487491d54c3f88c3731
jagesi <lic.yanhil316@gmail.com>
2015-11-12 21:03:01 -0400

    prueba de versionamiento

--- ProyectoUno/src/com/jagesi/test/TestName.java
+++ ProyectoUno/src/com/jagesi/test/TestName.java
@@ -4,7 +4,8 @@ public class TestName {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hola pedro");
+		int x = 10;
+		System.out.println("hola pedro " + x);
 	}
 
 }
